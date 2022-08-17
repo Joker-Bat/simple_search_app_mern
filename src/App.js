51197c34a40c91aebe05d66c4e3b72492bcb7fa4
Joker-Bat <shanmugam091098@gmail.com>
2022-08-17 14:58:58 +0530

    Initial code for search app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
+
+import axios from "axios";
+
+import { useNavigate, useLocation } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const debounceTimer = useRef(null);
+
+  const [keywords, setKeywords] = useState(
+    new URLSearchParams(location.search)?.get("keywords") ?? ""
+  );
+  const [adsToShow, setAdsToShow] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (debounceTimer.current) clearTimeout(debounceTimer.current);
+
+    debounceTimer.current = setTimeout(async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(
+          `http://localhost:3005/api/ad?keywords=${keywords}`
+        );
+        setAdsToShow(data.data.ads);
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+        console.error(`Error occured, Message: ${err.message}`);
+      }
+    }, 500);
+
+    return () => clearTimeout(debounceTimer.current);
+  }, [keywords]);
+
+  const handleChange = (e) => {
+    const { name, value } = e?.target;
+    const query = new URLSearchParams({ [name]: value });
+    navigate(
+      { pathname: location.pathname, search: query.toString() },
+      { replace: true }
+    );
+    setKeywords(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="search"
+        name="keywords"
+        placeholder="Enter keywords ..."
+        value={keywords}
+        onChange={handleChange}
+        className="SearchField"
+        autoComplete="off"
+      />
+
+      <div className="Content">
+        {loading && <div className="Spinner"></div>}
+
+        <div className="AdsGrid">
+          {adsToShow?.map((ad) => {
+            return (
+              <div className="Ad" key={ad._id}>
+                <img src={ad.imageUrl} alt={ad.primaryText} />
+                <h2 className="Title">{ad.headline}</h2>
+                <summary>
+                  <details>
+                    <p>{ad.primaryText}</p>
+                    <p>{ad.description}</p>
+                  </details>
+                </summary>
+                <a
+                  className="Cta"
+                  target="_blank"
+                  href={ad.companyId.url}
+                  rel="noreferrer"
+                >
+                  {ad.cta}
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
